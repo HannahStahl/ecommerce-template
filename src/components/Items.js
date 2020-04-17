7f7b5e3457fa1986e6ef296bc33961d28a6b2430
Hannah Stahl <hannah@oleaedge.com>
2020-04-17 11:51:30 -0500

    Added items from CMS to items page

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import config from '../config';
 
-const Items = () => (
-  <div>
-    <h1>Items Page</h1>
-    <p>Insert items here.</p>
-  </div>
-);
+const Items = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`${config.apiURL}/publishedItems/${config.userID}`).then((res) => res.json()).then(setItems);
+  }, []);
+
+  return (
+    <div>
+      <h1>Items Page</h1>
+      <div className="items">
+        {items.map((item) => (
+          <div className="item">
+            {item.itemName}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Items;
